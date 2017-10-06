8b84161afafd3714e6fc41f082c72aca3d0de680
Alexander Sharikhin <anihirash@gmail.com>
2017-10-06 21:26:58 +0300

    upd

--- main.rkt
+++ main.rkt
@@ -7,145 +7,153 @@
 (require web-server/servlet
            web-server/servlet-env)
   
-  (provide http/get
-           http/post
-           http/delete
-           http/put
-           server/set-port
-           request/param
-           request/cookie
-           response/make
-           response/404
-           server/run)
+(provide http/get
+         http/post
+         http/delete
+         http/put
+         server/set-port
+         request/param
+         request/cookie
+         response/make
+         response/404
+         server/run)
 
-  ; Request handlers
-  (define handlers (make-hash))
-  ; Listening port
-  (define env/port 8000)
-  ; HTTP-params hash(for GET/POST etc. vars)
-  (define params (make-hash))
-  ; Cookies hash
-  (define cookies (make-hash))
+; Request handlers
+(define handlers (make-hash))
+; Listening port
+(define env/port 8000)
+; HTTP-params hash(for GET/POST etc. vars)
+(define params (make-hash))
+; Cookies hash
+(define cookies (make-hash))
+; Headers list
+(define headers '())
 
-  ; Creating empty hash-maps for request handlers
-  (map
-   (λ (element)
-     (hash-set! handlers element (make-hash)))
-   '(get post put delete))
+; Creating empty hash-maps for request handlers
+(map
+ (λ (element)
+   (hash-set! handlers element (make-hash)))
+ '(get post put delete))
 
-  ; Remember request handler
-  ; Params are - request method(get/post/put/delete), path("/", "/hello/:id/") and callback
-  (define (http/handler method path proc)
-    (hash-set! (hash-ref handlers method) path proc))
+; Adds header to http response
+(define (http/add-header header)
+  (set! headers (append headers header)))
 
-  ; GET request handler
-  (define (http/get path proc)
-    (http/handler 'get path proc))
+; Remember request handler
+; Params are - request method(get/post/put/delete), path("/", "/hello/:id/") and callback
+(define (http/handler method path proc)
+  (hash-set! (hash-ref handlers method) path proc))
 
-  ; POST request handler
-  (define (http/post path proc)
-    (http/handler 'post path proc))
+; GET request handler
+(define (http/get path proc)
+  (http/handler 'get path proc))
 
-  ; PUT request handler
-  (define (http/put path proc)
-    (http/handler 'put path proc))
+; POST request handler
+(define (http/post path proc)
+  (http/handler 'post path proc))
 
-  ; DELETE request handler
-  (define (http/delete path proc)
-    (http/handler 'delete path proc))
+; PUT request handler
+(define (http/put path proc)
+  (http/handler 'put path proc))
 
-  ; Http path template to regexp translation
-  (define (path->regexp path)
-    (regexp
-     (string-append
-      "^/"
-      (string-trim
-       (regexp-replace* #rx":[^\\/]+" path "([^/?]+)") "/")
-      "(?:\\?|$)")))
+; DELETE request handler
+(define (http/delete path proc)
+  (http/handler 'delete path proc))
 
-  ; Set listen port
-  (define (server/set-port [port 8000])
-    (set! env/port port))
+; Http path template to regexp translation
+(define (path->regexp path)
+  (regexp
+   (string-append
+    "^/"
+    (string-trim
+     (regexp-replace* #rx":[^\\/]+" path "([^/?]+)") "/")
+    "(?:\\?|$)")))
 
-  ; Get request parameter
-  (define (request/param name)
-    (hash-ref params name #f))
+; Set listen port
+(define (server/set-port [port 8000])
+  (set! env/port port))
 
-  ; Parse cookies to hash-map
-  (define (server/parse-cookies req)
-    (map (λ (cookie)
-           (hash-set! cookies
-                      (client-cookie-name cookie)
-                      (client-cookie-value cookie)))
-         (request-cookies req)))
+; Get request parameter
+(define (request/param name)
+  (hash-ref params name #f))
 
-  ; Get cookie by name
-  (define (request/cookie name)
-    (hash-ref cookies name #f))
+; Parse cookies to hash-map
+(define (server/parse-cookies req)
+  (map (λ (cookie)
+         (hash-set! cookies
+                    (client-cookie-name cookie)
+                    (client-cookie-value cookie)))
+       (request-cookies req)))
 
-  ; Make response
-  (define (response/make #:code [code 200]
-                         #:message [message #"OK"]
-                         #:seconds [seconds (current-seconds)]
-                         #:mime-type [mime-type TEXT/HTML-MIME-TYPE]
-                         #:headers [headers (list (make-header #"Cache-Control" #"no-cache"))]
-                         content)
-    (response/full code
-                   message
-                   seconds
-                   mime-type
-                   headers
-                   (list (string->bytes/utf-8 content))))
+; Get cookie by name
+(define (request/cookie name)
+  (hash-ref cookies name #f))
 
-  ; 404 Response
-  (define (response/404)
-    (response/make #:code 404 "Page not found"))
+; Make response
+(define (response/make #:code [code 200]
+                       #:message [message #"OK"]
+                       #:seconds [seconds (current-seconds)]
+                       #:mime-type [mime-type TEXT/HTML-MIME-TYPE]
+                       #:headers [headers (cond 
+                                            [(empty? headers) (list (make-header #"Cache-Control" #"no-cache"))]
+                                            [(else headers)])]
+                       content)
+  (response/full code
+                 message
+                 seconds
+                 mime-type
+                 headers
+                 (list (string->bytes/utf-8 content))))
 
-  ; Find handler 
-  (define (server/find-handler req)
-    (let* ((path (regexp-replace* #rx"\\?.*" (url->string (request-uri req)) ""))
-           (method
-            (case (request-method req)
-              [(#"GET") 'get]
-              [(#"POST") 'post]
-              [(#"PUT") 'put]
-              [(#"DELETE") 'delete]))
-           (handler-key
-            (findf (λ (key)  (regexp-match (path->regexp key) path))
-                   (hash-keys (hash-ref handlers method)))))
-      (list path handler-key method)))
+; 404 Response
+(define (response/404)
+  (response/make #:code 404 "Page not found"))
 
-  ; Makes response
-  (define (server/response path-handler-method req)
-    (let* ((path (car path-handler-method))
-           (handler-key (cadr path-handler-method))
-           (method (caddr path-handler-method)))
-      (case handler-key
-        [(#f) (response/404)]
-        [else
-         (let* ((keys (map
-                       (λ (match) (string->symbol (substring match 2)))
-                       (regexp-match* #rx"/:([^\\/]+)" handler-key)))
-                (pairs
-                 (for/list ([key keys]
-                            [val (cdr (regexp-match (path->regexp handler-key) path))])
-                   (cons key val))))
-           (set! params (make-hash (append pairs (url-query (request-uri req)))))
-           (server/parse-cookies req)
-           (let* ((handler (hash-ref handlers method))
-                  (response ((hash-ref handler handler-key #f) req)))
-             (if (response? response)
-                 response
-                 (response/make ((hash-ref handler handler-key #f) req)))))])))
-  ; Runs server
-  (define (server/run)
-    (serve/servlet
-     (λ (req)
-       (server/response
-        (server/find-handler req)
-        req))
-     #:launch-browser? #f
-     #:servlet-path "/"
-     #:port env/port
-     #:listen-ip #f
-     #:servlet-regexp #rx""))
+; Find handler 
+(define (server/find-handler req)
+  (let* ((path (regexp-replace* #rx"\\?.*" (url->string (request-uri req)) ""))
+         (method
+          (case (request-method req)
+            [(#"GET") 'get]
+            [(#"POST") 'post]
+            [(#"PUT") 'put]
+            [(#"DELETE") 'delete]))
+         (handler-key
+          (findf (λ (key)  (regexp-match (path->regexp key) path))
+                 (hash-keys (hash-ref handlers method)))))
+    (list path handler-key method)))
+
+; Makes response
+(define (server/response path-handler-method req)
+  (let* ((path (car path-handler-method))
+         (handler-key (cadr path-handler-method))
+         (method (caddr path-handler-method)))
+    (case handler-key
+      [(#f) (response/404)]
+      [else
+       (let* ((keys (map
+                     (λ (match) (string->symbol (substring match 2)))
+                     (regexp-match* #rx"/:([^\\/]+)" handler-key)))
+              (pairs
+               (for/list ([key keys]
+                          [val (cdr (regexp-match (path->regexp handler-key) path))])
+                 (cons key val))))
+         (set! params (make-hash (append pairs (url-query (request-uri req)))))
+         (server/parse-cookies req)
+         (let* ((handler (hash-ref handlers method))
+                (response ((hash-ref handler handler-key #f) req)))
+           (if (response? response)
+               response
+               (response/make ((hash-ref handler handler-key #f) req)))))])))
+; Runs server
+(define (server/run)
+  (serve/servlet
+   (λ (req)
+     (server/response
+      (server/find-handler req)
+      req))
+   #:launch-browser? #f
+   #:servlet-path "/"
+   #:port env/port
+   #:listen-ip #f
+   #:servlet-regexp #rx""))
